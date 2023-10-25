6554cd18849e8c5ec2cd4472c76ed35c31af1028
arim83 <jorammercado@pursuit.org>
2023-10-25 18:44:21 -0400

    edit comp added

---
+++ src/Components/SongEditForm.jsx
@@ -0,0 +1,120 @@
+import { useState, useEffect } from "react"
+import { useParams, useNavigate, Link } from "react-router-dom"
+import "./SongEditForm.css"
+const API = import.meta.env.VITE_BASE_URL
+
+function SongEditForm() {
+  let { index } = useParams()
+  const navigate = useNavigate()
+  const [song, setSong] = useState({
+    name: "",
+    artist: "",
+    album: "",
+    time: "",
+    is_favorite: false
+  })
+  
+  const handleTextChange = (event) => {
+    setSong({ ...song, [event.target.id]: event.target.value })
+  }
+
+  const handleCheckboxChange = () => {
+    setTransaction({ ...song, is_favorite: !song.is_favorite })
+  }
+
+  useEffect(() => {
+    fetch(`${API}/songs/${index}`)
+      .then(response => response.json())
+      .then(song => {
+        //console.log(song)
+        setSong(song)
+    })
+    .catch(() => navigate("/not-found"))
+  }, [index, navigate]);
+
+  const updateSong = () => {
+    const httpOptions = {
+      "method" : "PUT",
+      "body" : JSON.stringify(song),
+      "headers" : {
+        "Content-type" : "application/json"
+      }
+    }
+
+      fetch(`${API}/songs/${index}`, httpOptions)
+        .then(() => { 
+          alert(`${song.name} has been updated!`);
+          navigate(`/songs/${index}`)
+        })
+        .catch((err) => console.error(err))
+  }
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    updateTransaction()
+  }
+  return (
+    <div className="Edit">
+      
+      <form onSubmit={handleSubmit}>
+      <label className="name" htmlFor="name">Song: </label>
+        <input
+          id="name"
+          value={song.name}
+          type="text"
+          onChange={handleTextChange}
+          placeholder="song"
+          required
+        />
+        <br></br>
+        <label className="artist" htmlFor="artist">Artist:</label>
+        <input
+          id="artist"
+          value={song.artist}
+          type="text"
+          onChange={handleTextChange}
+          placeholder="artist"
+          required
+        />
+        <br></br>
+        <label className="album" htmlFor="album">Artist:</label>
+        <input
+          id="album"
+          value={song.album}
+          type="text"
+          onChange={handleTextChange}
+          placeholder="album"
+        />
+        <br></br>
+        <label className="time" htmlFor="time">RunTime:</label>
+        <input
+          id="time"
+          value={song.time}
+          type="text"
+          onChange={handleTextChange}
+          placeholder="run time"
+        />
+        <br></br>
+        <label id="is_favorite" htmlFor="is_favorite">Favorite:</label>
+        <input
+          id="is_favorite"
+          type="checkbox"
+          checked={song.is_favorite}
+          onChange={handleCheckboxChange}
+        />
+        <br />
+        <input type="submit" />
+      </form>
+
+      <br></br>
+      <Link to={`/songs/${index}`}>
+        <button>Cancel</button>
+      </Link>
+      <Link to={`/songs`}>
+        <button>View All</button>
+      </Link>
+      
+    </div>
+  )
+}
+
+export default SongEditForm
